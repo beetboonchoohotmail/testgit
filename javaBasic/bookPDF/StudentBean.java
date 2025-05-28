93eb22767f94fc35be560ba7d50814ff4915fb47
beetsuttawathotmail <beet-lovepopy@hotmail.com>
2025-05-28 16:13:41 +0700

    test Jsp

---
+++ javaBasic/bookPDF/StudentBean.java
@@ -0,0 +1,26 @@
+public class StudentBean {
+    String fname;
+    String lname;
+    float gpa;
+
+    public String getFname() {
+        return fname;
+    }
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public float getGpa() {
+        return gpa;
+    }
+    public void setGpa(float gpa) {
+        this.gpa = gpa;
+    }
+}
