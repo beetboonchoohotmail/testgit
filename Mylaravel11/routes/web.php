0e04688cee039fc42ae322c234cf15cd8a06b164
beetsuttawathotmail <beet-lovepopy@hotmail.com>
2025-08-08 14:04:47 +0700

    ทำการสร้างใหม่

--- Mylaravel11/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-
-// import Controller Home
-use App\Http\Controllers\HomeController;
-//import PostController
-use App\Http\Controllers\PostsController;
