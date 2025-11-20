a4c0707846b3aa92a55bd5be8521d51ae61e700d
beetsuttawathotmail <beet-lovepopy@hotmail.com>
2025-11-20 15:54:01 +0700

    delete php basic

--- phpPDO/selectData.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include '../phpPDO/PDOConnect.php';
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email AND status = :status");
-    $stmt->execute(['email' => $email, 'status' => $status]);
-    $user = $stmt->fetch(); // ดึงข้อมูลหนึ่งแถว
-    $allUsers = $stmt->fetchAll(); // ดึงข้อมูลทั้งหมด
